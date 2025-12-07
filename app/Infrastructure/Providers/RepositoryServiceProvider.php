43f4776b06796940161e1ea946d9be28554a62be
MahmoudReda <mahreda19@gmail.com>
2025-12-07 10:37:09 +0200

    I want shear Adapter in fremwork laravel or Other I will using service continer Register . if start reques got to public shear if  any call CustomerRepository Refrance EloquentCustomerRepository  using dependency injection  in controler

---
+++ app/Infrastructure/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Infrastructure\Providers;
+
+use App\Domain\Customer\Repositories\CustomerRepository;
+use App\Infrastructure\Persistence\EloquentCustomerRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(CustomerRepository::class, EloquentCustomerRepository::class);
+    }
+}
